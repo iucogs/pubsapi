f5f248878a94d59015ee505370db3be5ec0d2eaa
Jaimie Murdock <jaimie.murdock@gmail.com>
2012-10-02 00:18:59 -0400

    adding comments

--- data.py
+++ data.py
@@ -6,16 +6,22 @@ import json
 from seppubs.model import *
 import seppubs.lib
 
+# create a placeholder for the JSON data
 citations = dict()
 
+# query for all citations
 for c in session.query(Citation).all():
+    # get the sep dirs from the sep collection names
     sep_entries = [col.collection_name.strip('SEP ')
         for col in c.collections if col.owner == 'sep']
-    citations[c.citation_id] = { 'sep_entries' : sep_entries,
+
+    # build the individual records
+    citations[c.citation_id] = { 'sep_dirs' : sep_entries,
         'possible_matches' : [match.citation_id 
             for match in c.possible_matches],
-        'record' : c.json,
+        'data' : c.json,
         'cleaned' : (c.entryTime != c.last_modified)
         }
 
+# simply print to the console to allow flexibility for file piping
 print seppubs.lib.json(citations)

87faa2df967e5a08e75486f24e9927ccd59acdc1
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-03-03 12:47:39 -0500

    adding argument parser

--- serve.py
+++ serve.py
@@ -4,5 +4,11 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='localhost', port=8080)
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('port', default=8080, type=int, nargs='?', help='port number')
+    args = parser.parse_args()
+
+    run(host='localhost', port=args.port)
 

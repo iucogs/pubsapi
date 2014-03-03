c14c88977d6debe40b995f4db892a9a2d0431264
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-03-03 13:01:41 -0500

    adding simple web server for citations

--- serve.py
+++ serve.py
@@ -1,8 +1,22 @@
-from bottle import route, run, template
+from bottle import abort, response, route, run
+import customjson
+
+from pubs.model import *
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
+# TODO: Add an @json decorator that sets content_type and pushes the function
+# return through customjson.dumps()
+@route('/citation/<id:int>')
+def get_citation(id):
+    response.content_type = 'application/json'
+
+    citation = Session.query(Citation).get(id)
+    if citation is None:
+        abort(400, "Citation not found")
+    return customjson.dumps(citation.json)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         ],
     install_requires=[
         "SQLAlchemy>=0.7.0,<=0.7.99",
+        "bottle>=0.12"
     ],
 
     long_description = """\
